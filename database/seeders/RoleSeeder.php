c6919da9610a9c50f45c5a0410cb5defc1dc777c
PhucHy <dat230300@gmail.com>
2025-08-16 15:39:48 +0700

    Phân quyền & tạo role cho admin

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+
 
 class RoleSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
     public function run(): void
     {
-        //
+        $admin = Role::firstOrCreate(['name' => 'Admin']);
+        $staff = Role::firstOrCreate(['name' => 'Staff']);
+
+        $user = User::firstOrCreate(
+            ['email' => 'admin@gmail.com'],
+            ['name' => 'Admin', 'password' => bcrypt('230300')]
+        );
+        $user->assignRole('Admin');
     }
 }
