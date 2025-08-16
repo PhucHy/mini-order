1ccc4f7e0c82faecba88bec580ad38054f683340
PhucHy <dat230300@gmail.com>
2025-08-16 16:36:34 +0700

    Tạo Models & mối quan hệ

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['customer_id', 'order_date', 'total_amount', 'status'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function tems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }   
+}
