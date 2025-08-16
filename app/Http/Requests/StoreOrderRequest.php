847563b2c86f309a8574f9473c5c47a3d28fc397
PhucHy <dat230300@gmail.com>
2025-08-17 00:05:57 +0700

    Validate

---
+++ app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'customer_id' => 'required|exists:customers,id',
+            'order_date' => 'required|date',
+
+            'items' => 'required|array|min:1',
+            'items.*.product_name' => 'required|string|max:255',
+            'items.*.quantity' => 'required|integer|min:1',
+            'items.*.price' => 'required|numeric|min:0',
+
+            'invoices.*' => 'file|mimes:pdf,jpg,jpeg,png|max:5120'
+        ];
+    }
+}
